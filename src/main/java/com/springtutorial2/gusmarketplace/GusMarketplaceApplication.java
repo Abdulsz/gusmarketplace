291f4a7e1b3ce9394b424f365571bac8c258f774
Abdul-Salam <abdul-salamzakaria22@augustana.edu>
2025-05-29 08:13:38 -0400

    Added dotenv for mongo uri

--- src/main/java/com/springtutorial2/gusmarketplace/GusMarketplaceApplication.java
+++ src/main/java/com/springtutorial2/gusmarketplace/GusMarketplaceApplication.java
@@ -1,14 +1,21 @@
 package com.springtutorial2.gusmarketplace;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
 public class GusMarketplaceApplication {
 
+
+
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+
         SpringApplication.run(GusMarketplaceApplication.class, args);
     }
 
